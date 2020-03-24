6ac05ca9e5c62542c30dc0a8948b2115aa2cdf65
Yehor Chernenko <yehor_chernenko@epam.com>
2020-03-24 12:07:57 +0200

    Add blur, spinner, clean up controller

--- CarPlate/Source/Modules/CarplateRecognition/CarPlateRecognitionViewController.swift
+++ CarPlate/Source/Modules/CarplateRecognition/CarPlateRecognitionViewController.swift
@@ -16,6 +16,8 @@ class CarPlateRecognitionViewController: UIViewController, UIImagePickerControll
     lazy var viewModel = CarPlateRecognitionViewModel(image: self.originalImage!)
     private var bindings = Set<AnyCancellable>()
 
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var blurImageView: UIImageView!
     @IBOutlet weak var recognizedTextLabel: UILabel!
     @IBOutlet weak var recognizedObjectImageView: UIImageView!
     @IBOutlet weak var imageView: UIImageView!
@@ -31,10 +33,10 @@ class CarPlateRecognitionViewController: UIViewController, UIImagePickerControll
     func didUpdateViewModelStateHandler(_ state: CarPlateRecognitionViewModel.State) {
         switch state {
         case .processing:
-            break
+            spinner.startAnimating()
 
         case .showBackgroundImage(let image):
-            break
+            blurImageView.image = image
 
         case .addDrawingLayer(let normalizedImage):
             imageView.image = normalizedImage
@@ -44,10 +46,11 @@ class CarPlateRecognitionViewController: UIViewController, UIImagePickerControll
             recognizedObjectImageView.image = image
 
         case .highlightRecognizedCarPlateRect(let boundingBox):
-            onObjectRecognized(rectOfRecognition: boundingBox)
+            highlightRecognized(rectOfInterest: boundingBox)
 
         case .showRecognizedText(let text):
             recognizedTextLabel.text = text
+            spinner.stopAnimating()
 
         case .didReceiveError(let message):
             assertionFailure("Error message: \(message)")
@@ -103,97 +106,25 @@ class CarPlateRecognitionViewController: UIViewController, UIImagePickerControll
         drawingLayer.anchorPoint = CGPoint.zero
         drawingLayer.position = CGPoint(x: xLayer, y: yLayer)
         drawingLayer.opacity = 0.5
-        drawingLayer.borderColor = UIColor.red.cgColor
-        drawingLayer.borderWidth = 2
+//        drawingLayer.borderColor = UIColor.red.cgColor
+//        drawingLayer.borderWidth = 2
         pathLayer = drawingLayer
-        self.view.layer.addSublayer(pathLayer!)
+        self.view.layer.addSublayer(drawingLayer)
     }
     
     // MARK: - Vision
 
-    func onObjectRecognized(rectOfRecognition: CGRect) {
+    func highlightRecognized(rectOfInterest: CGRect) {
         //draw path and set recognized image
         guard let drawLayer = self.pathLayer else { fatalError() }
-        draw(rectOfRecognition: rectOfRecognition, onImageWithBounds: drawLayer.bounds)
+        draw(rectOfRecognition: rectOfInterest, onImageWithBounds: drawLayer.bounds)
         drawLayer.setNeedsDisplay()
-
-//        guard let recognizedObjectImage = recognizedObjectImageView.image else { fatalError() }
-//
-//        viewModel.ocr(on: recognizedObjectImage) { [weak self] texts in
-//            self?.recognizedTextLabel.text = texts.first ?? ""
-//        }
     }
     
     // MARK: - Path-Drawing
     
-    fileprivate func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
-        
-        let imageWidth = bounds.width
-        let imageHeight = bounds.height
-        
-        // Begin with input rect.
-        var rect = forRegionOfInterest
-        
-        // Reposition origin.
-        rect.origin.x *= imageWidth
-        rect.origin.x += bounds.origin.x
-        rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
-        
-        // Rescale normalized coordinates.
-        rect.size.width *= imageWidth
-        rect.size.height *= imageHeight
-        
-        return rect
-    }
-    
-    fileprivate func normalizedRect(forRegionOfInterest: CGRect, originalImage: CGImage) -> CGRect {
-        let originalImageWidth = CGFloat(originalImage.width)
-        let originalImageHeight = CGFloat(originalImage.height)
-        // Begin with input rect.
-        var rect = forRegionOfInterest
-        
-        // Reposition origin.
-        rect.origin.x *= originalImageWidth
-        rect.origin.y =  (1 - rect.origin.y - rect.size.height) * originalImageHeight
-        
-        // Rescale normalized coordinates.
-        rect.size.width *= originalImageWidth
-        rect.size.height *= originalImageHeight
-        
-        return rect
-    }
-    
-    fileprivate func shapeLayer(color: UIColor, frame: CGRect) -> CAShapeLayer {
-        // Create a new layer.
-        let layer = CAShapeLayer()
-        
-        // Configure layer's appearance.
-        layer.fillColor = nil // No fill to show boxed object
-        layer.shadowOpacity = 0
-        layer.shadowRadius = 0
-        layer.borderWidth = 2
-        
-        // Vary the line color according to input.
-        layer.borderColor = color.cgColor
-        
-        // Locate the layer.
-        layer.anchorPoint = .zero
-        layer.frame = frame
-        layer.masksToBounds = true
-        
-        // Transform the layer to have same coordinate system as the imageView underneath it.
-        layer.transform = CATransform3DMakeScale(1, -1, 1)
-        
-        return layer
-    }
-    
-    fileprivate func draw(rectOfRecognition: CGRect, onImageWithBounds bounds: CGRect) {
+    func draw(rectOfRecognition: CGRect, onImageWithBounds bounds: CGRect) {
         CATransaction.begin()
-//        guard let originalImage = imageView.image?.cgImage else { fatalError() }
-//        let rect = normalizedRect(forRegionOfInterest: rectOfRecognition, originalImage: originalImage)
-//
-//        guard let recognizedImage = originalImage.cropping(to: rect) else { fatalError() }
-//        recognizedObjectImageView.image = UIImage(cgImage: recognizedImage)
 
         let rectBox = boundingBox(forRegionOfInterest: rectOfRecognition, withinImageBounds: bounds)
         let rectLayer = shapeLayer(color: .green, frame: rectBox)
@@ -205,20 +136,47 @@ class CarPlateRecognitionViewController: UIViewController, UIImagePickerControll
     }
 
 
-    // MARK: - Helper Methods
+    func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
 
-    func presentAlert(_ title: String, error: NSError) {
-        // Always present alert on main thread.
-        DispatchQueue.main.async {
-            let alertController = UIAlertController(title: title,
-                                                    message: error.localizedDescription,
-                                                    preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK",
-                                         style: .default) { _ in
-                                            // Do nothing -- simply dismiss alert.
-            }
-            alertController.addAction(okAction)
-            self.present(alertController, animated: true, completion: nil)
-        }
+        let imageWidth = bounds.width
+        let imageHeight = bounds.height
+
+        // Begin with input rect.
+        var rect = forRegionOfInterest
+
+        // Reposition origin.
+        rect.origin.x *= imageWidth
+        rect.origin.x += bounds.origin.x
+        rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
+
+        // Rescale normalized coordinates.
+        rect.size.width *= imageWidth
+        rect.size.height *= imageHeight
+
+        return rect
+    }
+
+    func shapeLayer(color: UIColor, frame: CGRect) -> CAShapeLayer {
+        // Create a new layer.
+        let layer = CAShapeLayer()
+
+        // Configure layer's appearance.
+        layer.fillColor = nil // No fill to show boxed object
+        layer.shadowOpacity = 0
+        layer.shadowRadius = 0
+        layer.borderWidth = 2
+
+        // Vary the line color according to input.
+        layer.borderColor = color.cgColor
+
+        // Locate the layer.
+        layer.anchorPoint = .zero
+        layer.frame = frame
+        layer.masksToBounds = true
+
+        // Transform the layer to have same coordinate system as the imageView underneath it.
+        layer.transform = CATransform3DMakeScale(1, -1, 1)
+
+        return layer
     }
 }
